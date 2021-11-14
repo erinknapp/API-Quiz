8407b92eb5ec610c6c5b8b3d7edd0adb55cc072a
Erin Knapp <erin.knapp@gmail.com>
2021-11-14 18:49:35 -0500

    added function to iterate through questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,11 +27,33 @@ function startGame () {
     getQuestions();
 };
 
+//pulling from questions.js
+
 var getQuestions = function () {
+
     var questionOne = questions[questionIndex];
 
-    var titleEl = document.getElementById 
+    var titleEl = document.getElementById ("nextQuestion");
+    titleEl.textContent = questionOne.title;
+    titleEl.style.fontSize = 250%;
+
+    optionsEl.innerHTML = "";
+
+    //interate through array of questions and answers
+    questionOne.options.forEach((option, i) =>{
+
+        var optionsButton = document.createElement("button")
+        optionsButton.setAttribute("class", "options");
+        optionsButton.setAttribute("value", "options");
+        optionsButton.style.color = "#66f1f1";
+        optionsButton.style.fontSize = "30px";
+        optionsButton.style.padding = "20px";
+
+        optionsButton.textContent = i + 1 + ". " + option;
+
+        optionsButton.onclick = questionBtn;
+        optionsEl.appendChild(optionsButton);
+     });
 }
-    
-}
+
 
