863ac20ddc1fdf91c19a447200afdf1511a6df76
Erin Knapp <erin.knapp@gmail.com>
2021-11-14 16:55:37 -0500

    restructured HTML and added another JS file for questions and answer

---
+++ assets/js/questions.js
@@ -0,0 +1,44 @@
+//Question and answer arrays for 4 questions
+
+var questions = [
+    {
+        title: "What does API stand for?",
+        options: [
+        "Apples Pears Independence", 
+        "Application Programming Interface", 
+        "I don't know", 
+        "Applied Private Investigator"],
+        answer: "Application Programming Interface"
+    },
+    {
+        title: "What does Javascript do?",
+        options:[
+        "I don't know", 
+        "Adds interactivity to a web page", 
+        "helps you choose the coffee you want", 
+        "decorates the web page"
+        ],
+        answer:"Adds interactivity to a web page" 
+
+    },
+    {
+        title: "Which of the following is NOT a best practice in JavaScript",
+        options: [
+            "Comment your code",
+            "Use function and variable names that tell you what they are for",
+            "Commit to Git regularly",
+            "None of the above"
+        ],
+        answer:"None of the above"
+    },   
+     {
+        title: "Is programming fun?",
+        options: [
+            "No",
+            "IDK",
+            "Yes of course!",
+            "I'd rather get a root canal"
+        ],
+        answer: "Yes of course!"
+    }               
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 //Declaration of variables needed using the getElementById method
 
-var startButton = document.getElementById (start-btn)
-var nextButton = document.getElementById (next-btn)
-var questionContainer = document.getElementById(questionCard)
-var questionEl = document.getElementById (question)
-var answerEl = document.getElementById (answer-buttons)
+var startButton = document.querySelector (start-btn);
+var nextButton = document.querySelector (next-btn);
+var questionContainer = document.gquerySelector (questionCard);
+var questionEl = document.querySelector  (question);
+var answerEl = document.querySelector  (answer-buttons);
 
 //Button click events
 startButton.addEventListener('click', startGame)
@@ -29,11 +29,3 @@ function selectAnswer () {
 
 }
 
-questions = [
-    {
-        question:
-        answers: [
-            
-        ]
-    }
-]
